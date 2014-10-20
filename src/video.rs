e148954fe42aefe473892a2ed324cef366b4bf59
woxtu <woxtup@gmail.com>
2014-10-21 00:28:52 +0900

    Add Frames

--- src/video.rs
+++ src/video.rs
@@ -1,5 +1,28 @@
+use ffi::core::*;
 use ffi::video::*;
 use ffi::types::{CvCapture};
+use image::Image;
+
+pub struct Frames<'a> {
+  capture: &'a Capture,
+  index: uint,
+  count: uint,
+}
+
+impl<'a> Iterator<Image> for Frames<'a> {
+  fn next(&mut self) -> Option<Image> {
+    if self.index < self.count {
+      let result = unsafe {
+        cvSetCaptureProperty(self.capture.raw, 1i32, self.index as f64);
+        Image { raw: cvCloneImage(cvQueryFrame(self.capture.raw)) }
+      };
+      self.index = self.index + 1u;
+      Some(result)
+    } else {
+      None
+    }
+  }
+}
 
 pub struct Capture {
   raw: *const CvCapture,
@@ -13,6 +36,13 @@ impl Capture {
         _ => Err(path_c_str.to_string()),
       }
     })
+  }
+
+  pub fn frames(&self) -> Frames {
+    let count = unsafe {
+      cvGetCaptureProperty(self.raw, 7i32)
+    } as uint;
+    Frames { capture: self, index: 0u, count: count }
   }
 }
 
