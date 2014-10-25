6454932440c8cdc89085f212c0bc92864e2989cf
woxtu <woxtup@gmail.com>
2014-10-25 22:11:50 +0900

    Remove some C functions

--- src/ffi/video.rs
+++ src/ffi/video.rs
@@ -5,15 +5,13 @@ use ffi::types::{CvCapture, CvSize, CvVideoWriter, IplImage};
 extern "C" {
   pub fn cvCreateFileCapture(filename: *const c_char) -> *const CvCapture;
   pub fn cvCreateCameraCapture(index: c_int) -> *const CvCapture;
-  pub fn cvGrabFrame(capture: *const CvCapture) -> c_int;
-  pub fn cvRetrieveFrame(capture: *const CvCapture, streamIdx: c_int) -> *const IplImage;
   pub fn cvQueryFrame(capture: *const CvCapture) -> *const IplImage;
-  pub fn cvReleaseCapture(capture: *const *const CvCapture) -> ();
+  pub fn cvReleaseCapture(capture: *const *const CvCapture);
   pub fn cvGetCaptureProperty(capture: *const CvCapture, property_id: c_int) -> c_double;
   pub fn cvSetCaptureProperty(capture: *const CvCapture, property_id: c_int, value: c_double) -> c_int;
   pub fn cvCreateVideoWriter(filename: *const c_char, fourcc: c_int, fps: c_double, frame_size: CvSize, is_color: c_int) -> *const CvVideoWriter;
   pub fn cvWriteFrame(writer: *const CvVideoWriter, image: *const IplImage) -> c_int;
-  pub fn cvReleaseVideoWriter(writer: *const *const CvVideoWriter) -> ();
+  pub fn cvReleaseVideoWriter(writer: *const *const CvVideoWriter);
 }
 
 pub static CV_CAP_PROP_POS_FRAMES: c_int = 1;
