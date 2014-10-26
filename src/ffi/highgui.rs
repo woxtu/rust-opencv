f23210eca8ab0fba8a98c7a525269825bbe543c7
woxtu <woxtup@gmail.com>
2014-10-27 01:57:53 +0900

    Add Trackbar

--- src/ffi/highgui.rs
+++ src/ffi/highgui.rs
@@ -3,22 +3,22 @@ use ffi::types::{CvMat, IplImage};
 
 #[link(name = "opencv_highgui")]
 extern "C" {
-  pub fn cvDestroyWindow(name: *const c_char) -> ();
+  pub fn cvDestroyWindow(name: *const c_char);
   pub fn cvNamedWindow(name: *const c_char, flags: c_int) -> c_int;
-  pub fn cvShowImage(name: *const c_char, image: *const IplImage) -> ();
-  pub fn cvResizeWindow(name: *const c_char, width: c_int, height: c_int) -> ();
-  pub fn cvMoveWindow(name: *const c_char, x: c_int, y: c_int) -> ();
+  pub fn cvShowImage(name: *const c_char, image: *const IplImage);
+  pub fn cvResizeWindow(name: *const c_char, width: c_int, height: c_int);
+  pub fn cvMoveWindow(name: *const c_char, x: c_int, y: c_int);
   pub fn cvCreateTrackbar2(trackbar_name: *const c_char,
                            window_name: *const c_char,
                            value: *const c_int,
                            count: c_int,
-                           on_change: extern "C" fn (c_int, *const c_void) -> (),
+                           on_change: extern "C" fn (pos: c_int, userdata: *const c_void),
                            userdata: *const c_void) -> c_int;
   pub fn cvGetTrackbarPos(trackbar_name: *const c_char, window_name: *const c_char) -> c_int;
-  pub fn cvSetTrackbarPos(trackbar_name: *const c_char, window_name: *const c_char, pos: c_int) -> ();
+  pub fn cvSetTrackbarPos(trackbar_name: *const c_char, window_name: *const c_char, pos: c_int);
   pub fn cvSetMouseCallback(window_name: *const c_char,
-                            on_mouse: extern "C" fn (c_int, c_int, c_int, c_int, *const c_void) -> (),
-                            param: *const c_void) -> ();
+                            on_mouse: extern "C" fn (event: c_int, x: c_int, y: c_int, flags: c_int, param: *const c_void),
+                            param: *const c_void);
   pub fn cvWaitKey(delay: c_int) -> c_int;
 
   pub fn cvDecodeImage(buf: *const CvMat, iscolor: c_int) -> *const IplImage;
--- src/highgui.rs
+++ src/highgui.rs
@@ -1,10 +1,40 @@
-use std::{mem, num};
+use std::{mem, num, ptr};
 use libc::{c_int, c_void};
 use ffi::highgui::*;
 use image::Image;
 
 pub fn wait_key(delay: int) -> int {
   unsafe { cvWaitKey(delay as i32) as int }
+}
+
+pub struct Trackbar<'a> {
+  name: String,
+  window: String,
+  on_change: Option<|uint|: 'a>,
+}
+
+impl<'a> Trackbar<'a> {
+  pub fn position(&self) -> uint {
+    self.name.with_c_str(|trackbar_name| {
+      self.window.with_c_str(|window_name| unsafe {
+        cvGetTrackbarPos(trackbar_name, window_name) as uint
+      })
+    })
+  }
+
+  pub fn set_position(&mut self, position: uint) {
+    self.name.with_c_str(|trackbar_name| {
+      self.window.with_c_str(|window_name| unsafe {
+        cvSetTrackbarPos(trackbar_name, window_name, position as i32);
+      })
+    })
+  }
+}
+
+impl<'a> Clone for Trackbar<'a> {
+  fn clone(&self) -> Trackbar<'a> {
+    Trackbar { name: self.name.clone(), window: self.window.clone(), on_change: None }
+  }
 }
 
 #[deriving(FromPrimitive)]
@@ -25,43 +55,62 @@ pub enum MouseEvent {
 
 pub struct Window<'a> {
   name: String,
-  on_mouse: Option<|MouseEvent, int, int|: 'a -> ()>,
+  trackbars: Vec<Trackbar<'a>>,
+  on_mouse: Option<|MouseEvent, int, int|: 'a>,
 }
 
 impl<'a> Window<'a> {
   pub fn named(name: &str) -> Result<Window, String> {
     name.with_c_str(|name_c_str| unsafe {
       match cvNamedWindow(name_c_str, 1i32) {
-        0 => Ok(Window { name: name.to_string(), on_mouse: None }),
+        0 => Ok(Window { name: name.to_string(), trackbars: Vec::new(), on_mouse: None }),
         _ => Err(name.to_string()),
       }
     })
   }
 
-  pub fn show(&self, image: &Image) -> () {
+  pub fn show(&self, image: &Image) {
     self.name.with_c_str(|name| unsafe {
       cvShowImage(name, image.ptr());
     })
   }
 
-  pub fn move_(&self, x: int, y: int) -> () {
+  pub fn move_(&self, x: int, y: int) {
     self.name.with_c_str(|name| unsafe {
       cvMoveWindow(name, x as i32, y as i32);
     })
   }
 
-  pub fn resize(&self, width: int, height: int) -> () {
+  pub fn resize(&self, width: int, height: int) {
     self.name.with_c_str(|name| unsafe {
       cvResizeWindow(name, width as i32, height as i32);
     })
   }
 
-  pub fn on_mouse(&mut self, on_mouse: |event: MouseEvent, x: int, y: int|: 'a -> ()) -> () {
-    extern "C" fn wrapper(event: c_int, x: c_int, y: c_int, _: c_int, param: *const c_void) -> () {
+  pub fn create_trackbar(&mut self, name: &str, position: uint, max: uint, on_change: |position: uint|: 'a) -> Trackbar<'a> {
+    extern "C" fn wrapper(pos: c_int, userdata: *const c_void) {
+      let callback = unsafe { mem::transmute::<_, &mut |uint|>(userdata) };
+
+      (*callback)(pos as uint);
+    }
+
+    self.trackbars.push(Trackbar { name: name.to_string(), window: self.name.clone(), on_change: Some(on_change) });
+    let trackbar = self.trackbars.last().unwrap();
+
+    trackbar.name.with_c_str(|trackbar_name| {
+      trackbar.window.with_c_str(|window_name| unsafe {
+        cvCreateTrackbar2(trackbar_name, window_name, ptr::null(), max as i32, wrapper, mem::transmute(trackbar.on_change.as_ref().unwrap()));
+        cvSetTrackbarPos(trackbar_name, window_name, position as i32);
+      })
+    });
+
+    trackbar.clone()
+  }
+
+  pub fn on_mouse(&mut self, on_mouse: |event: MouseEvent, x: int, y: int|: 'a) {
+    extern "C" fn wrapper(event: c_int, x: c_int, y: c_int, _: c_int, param: *const c_void) {
       let event = num::from_i32::<MouseEvent>(event).unwrap();
-      let callback = unsafe {
-        mem::transmute::<_, &mut |MouseEvent, int, int| -> ()>(param)
-      };
+      let callback = unsafe { mem::transmute::<_, &mut |MouseEvent, int, int|>(param) };
 
       (*callback)(event, x as int, y as int);
     }
@@ -75,7 +124,7 @@ impl<'a> Window<'a> {
 
 #[unsafe_destructor]
 impl<'a> Drop for Window<'a> {
-  fn drop(&mut self) -> () {
+  fn drop(&mut self) {
     self.name.with_c_str(|name| unsafe {
       cvDestroyWindow(name);
     });
